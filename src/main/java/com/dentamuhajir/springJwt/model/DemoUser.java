310a161d994e3826900ee170db022e4e4551cd68
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-01 19:05:23 +0700

    How to use Setter getter and private variable access

---
+++ src/main/java/com/dentamuhajir/springJwt/model/DemoUser.java
@@ -0,0 +1,9 @@
+package com.dentamuhajir.springJwt.model;
+
+public class DemoUser {
+    public void demo() {
+        User user = new User();
+        user.firstName = "Denta";
+        user.setPassword("secretpassword");
+    }
+}
---
+++ src/main/java/com/dentamuhajir/springJwt/repository/DemoRepository.java
@@ -0,0 +1,10 @@
+package com.dentamuhajir.springJwt.repository;
+
+import com.dentamuhajir.springJwt.model.User;
+
+public class DemoRepository {
+    public void demo() {
+        User user = new User();
+        user.setPassword("setpassword");
+    }
+}
