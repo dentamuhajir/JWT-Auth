155bd72d0bba369e25e9e6ffda88813928839c22
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-01 20:31:23 +0700

    Add UserDetailImpl service

---
+++ src/main/java/com/dentamuhajir/springJwt/service/UserDetailsImpl.java
@@ -0,0 +1,22 @@
+package com.dentamuhajir.springJwt.service;
+
+import com.dentamuhajir.springJwt.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserDetailsImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+}
