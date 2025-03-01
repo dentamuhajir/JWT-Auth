a5f8bfd8ccb44c2f8800130f387825f31f18125e
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-01 12:33:56 +0700

    Init Project

---
+++ src/test/java/com/dentamuhajir/springJwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentamuhajir.springJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
