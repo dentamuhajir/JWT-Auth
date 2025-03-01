2898bbeac0dd71cee4272cea5bb4566e4319534c
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-01 15:09:01 +0700

    Implement UserDetails Spring security

--- src/main/java/com/dentamuhajir/springJwt/model/User.java
+++ src/main/java/com/dentamuhajir/springJwt/model/User.java
@@ -1,10 +1,14 @@
 package com.dentamuhajir.springJwt.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
             @GeneratedValue(strategy = GenerationType.IDENTITY)
             @Column(name = "id")
@@ -45,8 +49,38 @@ public class User {
         this.lastName = lastName;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 
     public void setPassword(String password) {
