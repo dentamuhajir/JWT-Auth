019f5692020d249ab1efb6af100d10fbf44a44f7
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-05 00:15:07 +0700

    Add Authentication Service and Response

---
+++ src/main/java/com/dentamuhajir/springJwt/model/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.dentamuhajir.springJwt.model;
+
+public class AuthenticationResponse {
+    private  String token;
+
+    public AuthenticationResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
